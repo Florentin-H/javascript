2ae2eb3a1060ecfa1680aaa7f794784571cfa3cb
Florentin-H <florentin.harmand@hotmail.com>
2023-02-03 11:55:15 +0100

    morpion terminé reste à gérer la fin du jeu

--- morpion/script.js
+++ morpion/script.js
@@ -1,19 +1,66 @@
 const squares = document.querySelectorAll('button')
+const reset = document.querySelector('.reset')
+const winnerZone = document.querySelector('.winnerZone')
 const symbols = ['x','o']
 let tour = 0
-
+let draw = 0
 
 for (const square of squares ){
     
     square.addEventListener('click', function(){
         if(square.innerHTML.length == 0){
             this.innerHTML = symbols[tour]
+            win = tour
             tour++
-            tour %= 2
-
+            tour= tour% 2
+            draw++
+            
+            if (isVictory(squares)) {
+                winnerZone.innerHTML = symbols[win] + " a gagné";
+			}else if (draw == 9) {
+                winnerZone.innerHTML = " Match nul";
+                ;
+            }
         };
+        
      
         })
-
-    
 }
+
+function isVictory(squares) {
+	let combs = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let comb of combs) {
+		if (
+			squares[comb[0]].textContent == squares[comb[1]].textContent &&
+			squares[comb[1]].textContent == squares[comb[2]].textContent &&
+			squares[comb[0]].textContent != ''
+		) {
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+    reset.addEventListener('click', function(){
+        for(const square of squares){
+                    square.innerHTML = ""
+                    tour =1
+                    reset.innerHTML = "reset"
+                    winnerZone.innerHTML = ""
+                    draw = 0
+            }
+    })
+        
+
+
